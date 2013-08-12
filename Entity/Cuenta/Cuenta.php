076dedb582a421d9203d61bbb76e7ed706dcf713
juanh <juanh@juanh-Lenovo-G470>
2013-08-12 15:46:17 -0300

    fix relationship

--- Entity/Cuenta/Cuenta.php
+++ Entity/Cuenta/Cuenta.php
@@ -24,7 +24,7 @@ class Cuenta {
     private $descripcion;
 
     /**
-     * @ORM\ManyToOne(targetEntity="Snappminds\ContableBundle\Entity\TipoCuenta\TipoCuenta", inversedBy="cuentas"})
+     * @ORM\ManyToOne(targetEntity="Snappminds\ContableBundle\Entity\TipoCuenta\TipoCuenta")
      * @ORM\JoinColumn(name="cuentas_id", referencedColumnName="id")
      */
     protected $tipoCuenta;
